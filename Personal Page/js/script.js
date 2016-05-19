4610a6423dbae04edcb464ef7a3462ea3e6aaa15
keppa <keppa0911@hotmail.com>
2016-05-20 07:50:36 +0800

    Add files via upload

---
+++ Personal Page/js/script.js
@@ -0,0 +1,14 @@
+$(document).alton({
+// The full height container
+	fullSlideContainer: 'full', 
+// The full height slide class
+	singleSlideClass: 'slide',
+// Enable side navigation
+	useSlideNumbers: false,
+// Set the outside color of the pagination items
+	slideNumbersBorderColor: '#fff',
+// Set the inner color of the pagination items (not active)
+	slideNumbersColor: 'transparent',
+// The body class
+	bodyContainer: 'pageWrapper',
+});
